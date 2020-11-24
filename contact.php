7fb5206b7e313eddbfcde52b724ea08df334e91f
devlog-t <devlog.code@gmail.com>
2020-11-24 20:26:57 +0100

    init-contact

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <title>Gallico Basket - Association de Basketball à Vannes</title>
+</head>
+<body>
+
+    <!-- ===== HEADER ===== -->
+    <?php
+        include ('header.php');
+    ?>
+    
+    <main class="main">
+
+        <section class="contact__section bd-grid" id="contact">
+            
+        </section>
+    </main>
+
+    <!--===== FOOTER =====-->
+    <?php
+        include ('footer.php');
+    ?>
+    
+</body>
+</html>
--- header.php
+++ header.php
@@ -10,9 +10,9 @@
         
         <div class="nav__menu" id="nav-menu">
             <ul class="nav__list">
-                <li class="nav__item"><a href="#" class="nav__link">Le club</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">News</a></li>
-                <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+                <li class="nav__item"><a href="club.php" class="nav__link">Le club</a></li>
+                <li class="nav__item"><a href="blog.php" class="nav__link">News</a></li>
+                <li class="nav__item"><a href="contact.php" class="nav__link">Contact</a></li>
             </ul>
             <div class="social__bar">
                 <a href="#" class="social__icon"><i class='bx bxl-facebook'></i></a>

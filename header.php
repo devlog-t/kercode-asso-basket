735a5687690030d2d24c5aaf9652e50b27e8406f
Tom <devlog.code@gmail.com>
2020-11-24 18:07:19 +0100

    'init-php'

---
+++ header.php
@@ -0,0 +1,24 @@
+<!-- ===== HEADER ===== -->
+<header class="header">
+    <nav class="nav bd-grid">
+        <div class="nav__logo-wrapper">
+            <a href="#" class="nav__logo">Gallico Basket</a>
+        </div>
+        
+        <label for="nav-checkbox" class="nav__toggle"><i class='bx bx-menu'></i></label>
+        <input type="checkbox" class="nav__checkbox" id="nav-checkbox">
+        
+        <div class="nav__menu" id="nav-menu">
+            <ul class="nav__list">
+                <li class="nav__item"><a href="#" class="nav__link">Le club</a></li>
+                <li class="nav__item"><a href="#" class="nav__link">News</a></li>
+                <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+            </ul>
+            <div class="social__bar">
+                <a href="#" class="social__icon"><i class='bx bxl-facebook'></i></a>
+                <a href="#" class="social__icon"><i class='bx bxl-instagram' ></i></a>
+                <a href="#" class="social__icon"><i class='bx bxl-twitter' ></i></a>
+            </div>
+        </div>           
+    </nav>
+</header>

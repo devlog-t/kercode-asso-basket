47a085eef9da33412c2245e86a6d386e00cde642
Tom <devlog.code@gmail.com>
2020-11-25 15:27:53 +0100

    club

---
+++ club.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <title>Gallico Basket - Association de Basketball à Vannes</title>
+</head>
+
+<body>
+
+    <!-- ===== HEADER ===== -->
+    <?php
+        include ('header.php');
+    ?>
+
+    <!-- ===== CLUB HOME ===== -->
+    <div class="page-header section">
+        <h1 class="contact__header__title">Le club</h1>
+        <p class="text-header-intro">Fan de basket depuis 2020</p>
+    </div>
+
+    <!-- ===== CONTACT CONTENT ===== -->
+    <main class="main">
+        <section class="club section bd-grid" id="contact">
+            <h2 class="section-title">Gallico Basket</h2>
+            <p class="blog__text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam modi tempore amet laudantium quis.
+            </p>
+            <div class="card__img">
+                <img src="assets/img/terrain-basket-exterieur.jpg" alt="">
+            </div> 
+            <p class="blog__text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam modi tempore amet laudantium quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minima nobis esse odio consectetur officia et incidunt beatae fuga dolorum saepe quas voluptatibus neque nulla alias nemo cumque, tempora commodi?
+            </p>
+            <p class="blog__text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam modi tempore amet laudantium quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi rerum, suscipit cumque dicta error asperiores dolor impedit, sapiente deserunt mollitia minus illo commodi ducimus incidunt quis saepe debitis ullam ipsa aliquid laborum sit iusto? Sequi, blanditiis. Quaerat illum ratione accusamus corporis excepturi quibusdam, provident quos necessitatibus. Nesciunt deleniti architecto velit!
+            </p>
+            <p class="blog__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, doloremque!
+            </p>
+            
+        </section>
+    </main>
+
+    <!--===== FOOTER =====-->
+    <?php
+        include ('footer.php');
+    ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/slider.css">
 
     <!-- ===== BOX ICONS ===== -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>

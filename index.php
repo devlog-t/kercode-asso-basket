2e78a1aae638ff883636629f50d916012d36530f
Tom <devlog.code@gmail.com>
2020-12-15 16:15:10 +0100

    minify

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ ob_start("ob_gzhandler");
     <link rel="stylesheet" href="/assets/css/main.css">
 
     <!-- ===== BOX ICONS ===== -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <!-- <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'> -->
 
     <title>Gallico Basket - Association de Basketball à Vannes</title>
 </head>
